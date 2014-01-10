4496dea5eef4a42b5739347e3cbfc7d6c2a8cd4a
pavlo.savushchyk <pavlo.savushchyk@globallogic.com>
2014-01-10 19:41:35 +0200

    rename logger

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ requirejs.config({
     baseUrl: "js",
     paths: {
         "QUnit": "libs/qunit-1.12.0",
-        "Logger": "jslog/logger",
+        "Logger": "Logger/Logger",
         "jasmine": "libs/jasmine",
         "jasmine-html": "libs/jasmine-html"
     },
