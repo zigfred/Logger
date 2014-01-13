577f5fb9b1101381d64f4fb24775d126e2fc9a03
pavlo.savushchyk <pavlo.savushchyk@globallogic.com>
2014-01-13 12:18:13 +0200

    fix levels, switch info and debug

--- js/Logger/level.js
+++ js/Logger/level.js
@@ -43,8 +43,8 @@ define(function (require) {
         return Level[level.toUpperCase()];
     };
     Level["ALL"] = new Level(Number.MIN_VALUE, "ALL");
-    Level["DEBUG"] = new Level(100, "DEBUG");
-    Level["INFO"] = new Level(200, "INFO");
+    Level["INFO"] = new Level(100, "INFO");
+    Level["DEBUG"] = new Level(200, "DEBUG");
     Level["WARN"] = new Level(300, "WARN");
     Level["ERROR"] = new Level(400, "ERROR");
     Level["OFF"] = new Level(Number.MAX_VALUE, "OFF");
